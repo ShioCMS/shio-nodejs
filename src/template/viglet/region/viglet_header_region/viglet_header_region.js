934e39d3ec543a45fed7d8da41cd386883d20f00
Alexandre Oliveira <alexandre.oliveira@sumware.com.br>
2020-05-26 21:15:54 -0300

    - Using await into templates

--- src/template/viglet/region/viglet_header_region/viglet_header_region.js
+++ src/template/viglet/region/viglet_header_region/viglet_header_region.js
@@ -1,36 +1,25 @@
 'use strict'
 var Handlebars = require('handlebars');
-
 async function render(shContent, shObject, html) {
     var template = Handlebars.compile(html);
-
-    var navigation = shObject.navigation("Viglet", true);
-    var forEach = Array.prototype.forEach;
+    var navigation = await shObject.navigation("Viglet", true);
+    var forEach = Array.prototype.forEachAsync;
     var currentFolder = new String(shContent.system.id);
     var folders = [];
-
-    forEach.call(navigation, function (shFolder) {
+    for (const shFolder of navigation) {
         var folderId = new String(shFolder.id);
-
         var isCurrentFolder = false;
-
         if (!currentFolder.localeCompare(folderId)) {
             isCurrentFolder = true;
         }
-
         var folder = {
-            "name": shFolder.name,
-            "link": shObject.generateFolderLink(shFolder.id),
-            "current": isCurrentFolder
+            "name": shFolder.name
+            , "link": await shObject.generateFolderLink(shFolder.id)
+            , "current": isCurrentFolder
         }
-
         folders.push(folder);
-    });
-
+    };
     var html = template(folders);
     return html;
-
 }
-
 module.exports.render = render;
-
