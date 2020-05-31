d93eca57486066287e2703f2b641699275c742cc
Alexandre Oliveira <alexandre.oliveira@sumware.com.br>
2020-05-30 21:00:21 -0300

    - Shio Static Files Proxy

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 'use strict'
 import { ShServer } from '@viglet/shio'
+const request = require('request');
 const express = require('express');
 
 const app = express();
@@ -13,9 +14,26 @@ const shServer = new ShServer(endpoint);
 app.use(express.static('public'))
 
 app.use(async function (req, res, next) {
-    var html = await shServer.getPage(req.originalUrl);
-    shioDebug(html);
-    res.send(html);    
-    return next();
+    var url = req.originalUrl;
+    var urlArray = url.split("/");
+    var context = urlArray[1];
+    if (context === 'store') {
+         var requestSettings = {
+                url: "http://localhost:2710" + url,
+                method: 'GET',
+                encoding: null
+            };
+
+            await request(requestSettings, function (error, response, body) {
+                res.set('Content-Type', 'image/png');
+                res.send(body);
+            });
+    }
+    else {
+        var html = await shServer.getPage(req.originalUrl, res);
+        shioDebug(html);
+        res.send(html);    
+
+    }
 });
 app.listen(port, () => console.log(`http://localhost:${port}`));
--- src/template/viglet/region/viglet_header_region/viglet_header_region.js
+++ src/template/viglet/region/viglet_header_region/viglet_header_region.js
@@ -1,23 +1,29 @@
-(async () => {
-    var Handlebars = require('handlebars');
-    var template = Handlebars.compile(html);
-    var navigation = await shObject.navigation("Viglet", true);
-    var forEach = Array.prototype.forEach;
-    var currentFolder = new String(shContent.system.id);
-    var folders = [];
-    for (const shFolder of navigation) {
-        var folderId = new String(shFolder.id);
-        var isCurrentFolder = false;
-        if (!currentFolder.localeCompare(folderId)) {
-            isCurrentFolder = true;
-        }
-        var folder = {
-            "name": shFolder.name
-            , "link": await shObject.generateFolderLink(shFolder.id)
-            , "current": isCurrentFolder
-        }
-        folders.push(folder);
-    };
-    html = template(folders);
-    html;
-})()
+var Handlebars = require('handlebars');
+var template = Handlebars.compile(html);
+
+var navigation = shObject.navigation("Viglet", true);
+var forEach = Array.prototype.forEach;
+var currentFolder = new String(shContent.system.id);
+var folders = [];
+
+forEach.call(navigation, function (shFolder) {
+    var folderId = new String(shFolder.id);
+    var folderName = new String(shFolder.name);
+    
+    var isCurrentFolder = false;
+    
+    if (!currentFolder.localeCompare(folderId)) {
+        isCurrentFolder = true;
+    }
+
+    var folder = {
+        "name" : shFolder.name,
+        "link" : shObject.generateFolderLink(shFolder.id),
+        "current": isCurrentFolder
+    }
+
+    folders.push(folder);
+});
+
+html = template(folders);
+html;
