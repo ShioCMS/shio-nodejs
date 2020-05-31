a1c90e1d9745512b12a4de0cf9356defe6fa6f13
Alexandre Oliveira <alexandre.oliveira@sumware.com.br>
2020-05-30 23:12:59 -0300

    - FileServer Property

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 'use strict'
 import { ShServer } from '@viglet/shio'
-const request = require('request');
-const express = require('express');
 
+const express = require('express');
+var urllib = require('urllib');
 const app = express();
 const shioDebug = require('debug')('shio:http')
 const propertiesReader = require('properties-reader');
 const properties = propertiesReader('./shio.properties');
-const port = properties.get('main.app.port');
-const endpoint = properties.get('main.app.shio.endpoint');
+const port = properties.get('app.port');
+const endpoint = properties.get('app.shio.endpoint');
+const fileServer = properties.get('app.shio.file.server');
 const shServer = new ShServer(endpoint);
 
 app.use(express.static('public'))
@@ -18,21 +19,21 @@ app.use(async function (req, res, next) {
     var urlArray = url.split("/");
     var context = urlArray[1];
     if (context === 'store') {
-         var requestSettings = {
-                url: "http://localhost:2710" + url,
-                method: 'GET',
-                encoding: null
-            };
 
-            await request(requestSettings, function (error, response, body) {
-                res.set('Content-Type', 'image/png');
-                res.send(body);
-            });
+        urllib.request(fileServer + url, function (err, data, response) {
+            if (err) {
+                throw err; // you need to handle error
+            }
+            console.log(response.statusCode);
+            console.log(response.headers);
+            // data is Buffer instance
+            res.send(data);
+        });
     }
     else {
         var html = await shServer.getPage(req.originalUrl, res);
         shioDebug(html);
-        res.send(html);    
+        res.send(html);
 
     }
 });
