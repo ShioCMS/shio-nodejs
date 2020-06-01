edd15c3f71390851e3d05393261c6e512cbe2580
Alexandre Oliveira <alexandre.oliveira@sumware.com.br>
2020-05-31 22:50:05 -0300

    - shioconfig.json file

--- src/index.js
+++ src/index.js
@@ -5,12 +5,7 @@ const express = require('express');
 var urllib = require('urllib');
 const app = express();
 const shioDebug = require('debug')('shio:http')
-const propertiesReader = require('properties-reader');
-const properties = propertiesReader('./shio.properties');
-const port = properties.get('app.port');
-const endpoint = properties.get('app.shio.endpoint');
-const fileServer = properties.get('app.shio.file.server');
-const shServer = new ShServer(endpoint);
+const shServer = new ShServer();
 
 app.use(express.static('public'))
 
@@ -19,22 +14,18 @@ app.use(async function (req, res, next) {
     var urlArray = url.split("/");
     var context = urlArray[1];
     if (context === 'store') {
-
-        urllib.request(fileServer + url, function (err, data, response) {
+        urllib.request(shServer.getFileServer() + url, function (err, data, response) {
             if (err) {
-                throw err; // you need to handle error
+                throw err;
             }
-            console.log(response.statusCode);
-            console.log(response.headers);
-            // data is Buffer instance
             res.send(data);
         });
     }
     else {
-        var html = await shServer.getPage(req.originalUrl, res);
+        var html = await shServer.getPage(req.originalUrl);
         shioDebug(html);
         res.send(html);
 
     }
 });
-app.listen(port, () => console.log(`http://localhost:${port}`));
+app.listen(shServer.getAppPort(), () => console.log(`http://localhost:${shServer.getAppPort()}`));
